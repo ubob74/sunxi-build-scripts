b0c53e5dfee7ebbda1e9a4a9774969ac377e37f3
Vladimir Meshkov <ubob74@gmail.com>
2018-12-20 22:53:43 +0300

    Add uboot spl installer
    Signed-off-by: Vladimir Meshkov <ubob74@gmail.com>

---
+++ u-boot/install_uboot_spl.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+. ../sunxirc
+
+source ../common.sh
+
+check_root_permission;
+if [ $? -ne 0 ]; then
+	exit 1;
+fi
+
+test -f $UBOOT_DIR/u-boot-sunxi-with-spl.bin || {
+	printf "No sunxi spl file\n";
+	exit 1
+}
+
+printf "ok\n"
+
+dd if=$UBOOT_DIR/u-boot-sunxi-with-spl.bin of=/dev/sdc bs=1024 seek=8
+
